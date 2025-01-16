9168541c572b7d32399833ed5353b463064c8171
rbijman <rik-bijman@hotmail.com>
2025-01-16 13:56:53 +0100

    Update AIweerfile_main.py
    added some seaborn plots

--- ALTEN/Files/Spyder scripts/AIweerfile_main.py
+++ ALTEN/Files/Spyder scripts/AIweerfile_main.py
@@ -4,6 +4,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 import datetime
 
 #%% Variable defitions
@@ -33,7 +34,7 @@ datac = pd.DataFrame(data["NLSitNummer"])
 datac['DateTimeStart'] = pd.to_datetime(data.DatumFileBegin+' '+data.TijdFileBegin)
 datac['DateTimeEnd'] = pd.to_datetime(data.DatumFileEind+' '+data.TijdFileEind)
 datac['Duration'] = data.FileDuur.str.replace(',','.').astype('float64')
-datac['Road'] = data.RouteOms.astype('category')
+datac['Road'] = data.RouteOms#.astype('category')
 datac['HPstart'] = data.HectometerKop.str.replace(',','.').astype('float64')
 datac['HPend'] = data.HectometerStaart.str.replace(',','.').astype('float64')
 datac['direction'] = data.hectometreringsrichting;
@@ -41,6 +42,7 @@ datac['HPrange'] = datac.apply(lambda x: getHPrange(x['HPstart'],x['HPend']),axi
 datac['ampm'] = datac.DateTimeStart.dt.time.apply(lambda x: 'am' if x<datetime.time(ampm_border) else 'pm')
 datac['shortlong'] = datac.Duration.apply(lambda x: 'short' if x<shortlong_border else 'long')
 datac['month'] = data.DatumFileBegin.apply(lambda x: x[5:7]).astype(int).astype('category')
+datac['date'] = datac.DateTimeStart.dt.date
 datac['season'] = pd.cut(datac.month,season_bins,right=True,labels=season_names)
 datac['weekday'] = datac.DateTimeStart.dt.dayofweek
 datac.head()
@@ -48,7 +50,6 @@ datac.tail()
 
 
 #JOIN WEATHER DATA
-#JOIN WEAKDAY INFO
 
 #%% plot the amount of trafic per month
 datac.sort_values(by='month').groupby('month',observed=True).Duration.count().plot(kind='bar')
@@ -85,7 +86,8 @@ plt.ylabel('counts')
 plt.show()
 
 #%% plot the amount of trafic per HP for a specific road splitted per am/pm
-road = 'A20'
+road = 'A27'
+nbins = 100
 gbo_per_road = datac.query('Road.str.contains("A")').groupby(['Road'],observed=True)
 gbo_per_road.get_group(road)
 am_oplopend = gbo_per_road.get_group(road).query('ampm=="am" & direction=="oplopend"').explode('HPrange')
@@ -96,28 +98,28 @@ pm_aflopend = gbo_per_road.get_group(road).query('ampm=="pm" & direction=="aflop
 plt.figure(figsize=(20,12))
 plt.suptitle(('{}').format(road))
 plt.subplot(2,2,1)
-plt.hist([am_oplopend.query('Duration<@shortlong_border')['HPrange'],pm_oplopend.query('shortlong=="short"')['HPrange']],bins=100,label=['am','pm'])
+plt.hist([am_oplopend.query('Duration<@shortlong_border')['HPrange'],pm_oplopend.query('shortlong=="short"')['HPrange']],bins=nbins,label=['am','pm'])
 plt.title('# of SHORT trafics splitted per am/pm - Oplopend')
 plt.ylabel('counts')
 plt.xlabel('Hectometer paal')
 plt.legend()
 
 plt.subplot(2,2,2)
-plt.hist([am_aflopend.query('Duration<@shortlong_border')['HPrange'],pm_aflopend.query('shortlong=="short"')['HPrange']],bins=100,label=['am','pm'])
+plt.hist([am_aflopend.query('Duration<@shortlong_border')['HPrange'],pm_aflopend.query('shortlong=="short"')['HPrange']],bins=nbins,label=['am','pm'])
 plt.title('# of SHORT trafics splitted per am/pm - Aflopend')
 plt.ylabel('counts')
 plt.xlabel('Hectometer paal')
 plt.legend()
 
 plt.subplot(2,2,3)
-plt.hist([am_oplopend.query('Duration>@shortlong_border')['HPrange'],pm_oplopend.query('shortlong=="long"')['HPrange']],bins=100,label=['am','pm'])
+plt.hist([am_oplopend.query('Duration>@shortlong_border')['HPrange'],pm_oplopend.query('shortlong=="long"')['HPrange']],bins=nbins,label=['am','pm'])
 plt.title('# of LONG trafics splitted per am/pm - Oplopend')
 plt.ylabel('counts')
 plt.xlabel('Hectometer paal')
 plt.legend()
 
 plt.subplot(2,2,4)
-plt.hist([am_aflopend.query('Duration>@shortlong_border')['HPrange'],pm_aflopend.query('shortlong=="long"')['HPrange']],bins=100,label=['am','pm'])
+plt.hist([am_aflopend.query('Duration>@shortlong_border')['HPrange'],pm_aflopend.query('shortlong=="long"')['HPrange']],bins=nbins,label=['am','pm'])
 plt.title('# of LONG trafics splitted per am/pm - Aflopend')
 plt.ylabel('counts')
 plt.xlabel('Hectometer paal')
@@ -127,4 +129,26 @@ plt.show()
 #plt.savefig(r"C:\Users\rbijman\Documents\GitHub\rgbijmanproductions\ALTEN\Files\Output\Plots\test.png")
 
 #%%
-datac.sort_values()
+plt.figure(figsize=(20,12))
+plt_data = datac.query('Road.str.contains("A12") & Duration>50')
+sns.stripplot(data=plt_data,y='Road',x='Duration',hue='month',dodge=True)
+plt.xticks(rotation=90)
+plt.show()
+
+#%% plot per day per road number of files
+road = 'A12'
+gbo_per_road = datac.groupby(['Road'],observed=True)
+hp=39.1
+plt_data = gbo_per_road.get_group(road).explode('HPrange')
+plt.hist([plt_data.query('HPrange==@hp & direction=="aflopend"').date,plt_data.query('HPrange==@hp & direction=="oplopend"').date],bins=365)
+plt.xticks(plt_data.date.sort_values().unique()[::10],rotation=90)
+plt.show()
+
+#%%
+max_counts = []
+max_hp = []
+for road in datac.Road.unique():
+    max_counts.append(datac.query('Road==@road').explode('HPrange').HPrange.value_counts().iloc[0])
+    max_hp.append(datac.query('Road==@road').explode('HPrange').HPrange.value_counts().index[0])
+    
+pd.DataFrame(list(zip(datac.Road.unique(),max_hp,max_counts)))
