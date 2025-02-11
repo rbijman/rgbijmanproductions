33de3a165f02a6099a42fa2a7a0eb1d1c767471f
rbijman <rik-bijman@hotmail.com>
2025-02-11 16:18:42 +0100

    Refactor and extend app_layout
    tabs are now all subfunctions
    new further analysis tab with extended functionality

--- ALTEN/Files/AIweerfiledashboard/Subfunctions/app_layout.py
+++ ALTEN/Files/AIweerfiledashboard/Subfunctions/app_layout.py
@@ -10,106 +10,180 @@ def main(app,dcc,html,datac,data):
     app.layout = html.Div(
         children=[
             
-            dcc.Tabs([
-                dcc.Tab(id='trafic',label='Trafic',children=[
-                    
-                    html.H1(children="Trafic Analytics"),
-                    html.P(
-                        children=(
-                            "Analyze all Trafics"
-                        ),
-                    ),
-                    html.Div(["Select a month: "]),
-    
-                    dcc.RadioItems(datac.index.month.unique(), 1,id='month_select_trafic',inline=True),
-                    
-                    dcc.DatePickerRange(
-                    id='date_range_trafic',
-                    min_date_allowed=datac.index.round('D').min(),
-                    max_date_allowed=datac.index.round('D').max(),
-                    initial_visible_month=datac.index.min(),
-                    start_date=datac.index.min(),
-                    end_date=datac.index.max()
-                    ),
-                    
-                    
-                    html.Div(["Select Road: "]),
-                    dcc.Dropdown(datac.Road.sort_values().unique(),'A1',id='road_select_trafic'),
-                    
-                    html.Div(["Split by: "]),
-    
-                    dcc.RadioItems(['no_split','direction','ampm','shortlong','weekday'], 'no_split',id='split_by_trafic',inline=True),
-    
-                    html.Div(["Graph1: "]),
-                    
-                    dcc.Loading(
-                    id='loading_trafic1',   
-                    children=[
-                     dcc.Graph(id="graph_trafic1"),   
-                        ]
-                    ),
-                    
-                    html.Div(["Graph2: "]),
-
-                    
-                    dcc.Loading(
-                    id='loading_trafic2',   
-                    children=[
-                      dcc.Graph(id="graph_trafic2"),   
-                        ]
-                    ),
-                    
-                    ]),
-                
-                dcc.Tab(id='weather_and_trafic',label='Weather+Trafic',children=[
-                    html.H1(children="Weather + Trafic Analytics"),
-                    html.P(
-                        children=(
-                            "Analyze weather + trafic"
-                        ),
-                    ),
-                    html.Div(["Select a month: "]),
-    
-                    dcc.RadioItems(datac.index.month.unique(), 1,id='month_select_weather',inline=True),
-                    
-                    dcc.DatePickerRange(
-                    id='date_range_weather',
-                    min_date_allowed=datac.index.round('D').min(),
-                    max_date_allowed=datac.index.round('D').max(),
-                    initial_visible_month=datac.index.min(),
-                    start_date=datac.index.min(),
-                    end_date=datac.index.max()
-                    ),
-                    
-                    
-                    html.Div(["Select City: "]),
-                    dcc.Dropdown(data.city.sort_values().unique(),'Gouda',id='city_select'),
-                    html.Div(["Select Road: "]),
-                    dcc.Dropdown(datac.Road.sort_values().unique(),'A1',id='road_select_weather'),
-                    
-                    html.Div(["Split by: "]),    
-                    dcc.RadioItems(['no_split','direction','ampm','shortlong','weekday'], 'no_split',id='split_by_weather',inline=True),
-                    
-                    html.Div(["Weather type: "]),
-    
-                    dcc.RadioItems(['temperature_2m','rain'], 'temperature_2m',id='weather_type',inline=True),
-    
-                    
-                    dcc.Loading(
-                    id='loading_weather',   
-                    children=[
-                      dcc.Graph(id="graph_weather"),   
-                        ]
-                    ),
-                    
-                    
-                    
-                    ]),
-                
-                ])
-            
-    
-            
+            dcc.Tabs(value='further',children=[
+                trafic_tab(dcc,html,datac),
+                weather_tab(dcc,html,data,datac),
+                further_tab(dcc,html,data,datac)
+                ])  
         ]
     )
     return app
+
+
+def trafic_tab(dcc,html,datac):
+    tab_name = 'trafic'
+    trafic_tab = dcc.Tab(id=tab_name,value=tab_name,label='Trafic',children=[
+    
+    html.H1(children="Trafic Analytics"),
+    html.P(
+        children=(
+            "Analyze all Trafics"
+        ),
+    ),
+ 
+    html.Div(["Select a month: "]),
+    __month_picker(dcc,datac,tab_name),    
+    html.Div(["Or select a specific date range: "]),
+    __date_picker(dcc,datac,tab_name),    
+
+    html.Div(["Select road: "]),
+    __road_picker(dcc, datac, tab_name),
+    
+    html.Div(["Split by: "]),
+    __split_by_picker(dcc,tab_name),
+
+    html.Div(["Graph1: "]),
+    
+    dcc.Loading(
+    id='loading_trafic1',   
+    children=[
+     dcc.Graph(id=("graph_{}1").format(tab_name)),   
+        ]
+    ),
+    
+    html.Div(["Graph2: "]),
+    
+    dcc.Loading(
+    id='loading_trafic2',   
+    children=[
+      dcc.Graph(id=("graph_{}2").format(tab_name)),   
+        ]
+    ),
+    
+    ])
+    return trafic_tab
+
+def weather_tab(dcc,html,data,datac):
+    tab_name = 'weather_and_trafic'
+    weather_tab = dcc.Tab(id=tab_name,value=tab_name,label='Weather+Trafic',children=[
+        html.H1(children="Weather + Trafic Analytics"),
+        html.P(
+            children=(
+                "Analyze weather + trafic"
+            ),
+        ),
+
+         html.Div(["Select a month: "]),
+        __month_picker(dcc,datac,tab_name),            
+        html.Div(["Or select a specific date range: "]),
+        __date_picker(dcc,datac,tab_name),            
+                
+        html.Div(["Select road: "]),
+        __road_picker(dcc, datac, tab_name),           
+        html.Div(["Select city: "]),
+        __city_picker(dcc,data,tab_name),        
+        
+        html.Div(["Split by: "]),    
+        __split_by_picker(dcc,tab_name),
+        
+        html.Div(["Select weather type: "]),
+        __weather_type_picker(dcc, data, tab_name),
+
+        dcc.Loading(
+        id='loading_weather',   
+        children=[
+          dcc.Graph(id=("graph_{}").format(tab_name)),   
+            ]
+        ),
+        
+        ])
+    return weather_tab
+
+def further_tab(dcc,html,data,datac):
+    tab_name = 'further'
+    new_tab = dcc.Tab(id=tab_name,value=tab_name,label='further',children=[
+        html.H1(children="further"),
+        html.P(
+            children=(
+                "Further analyses"
+            ),
+        ),
+        html.Div(["Select a month: "]),
+        __month_picker(dcc,datac,tab_name),             
+        html.Div(["Or select a specific date range: "]),
+        __date_picker(dcc,datac,tab_name),             
+    
+        html.Div(["Select road: "]),
+        __road_picker(dcc, datac, tab_name),             
+        html.Div(["Select city: "]),
+        __city_picker(dcc,data,tab_name), 
+                  
+         html.Div(["Select weather type: "]),
+         __weather_type_picker(dcc, data, tab_name),
+         
+         html.Div(["Select trafic information: "]),
+         __trafic_information_picker(dcc,tab_name),
+         
+         html.Div(["Select statistic: "]),
+         __statistic_picker(dcc,tab_name),
+         
+          html.Div(["Select frequency: "]),
+          __frequence_picker(dcc,tab_name),
+         
+         html.Div(["Select weekdays of interest: "]),
+         __weekday_picker(dcc,tab_name),
+
+        html.Div(["Select rootcause: "]),
+        __rootcause_picker(dcc,datac,tab_name),
+
+        
+        dcc.Loading(
+        id='loading_further',   
+        children=[
+          dcc.Graph(id="graph_further"),   
+            ]
+        ),
+        
+        ])
+    return new_tab
+
+
+def __month_picker(dcc,datac,tab_name):
+    return dcc.Dropdown(datac.index.month.unique(), 1,id=('month_select_{}').format(tab_name),style={'width': '20%'})
+
+def __date_picker(dcc,datac,tab_name):
+    return dcc.DatePickerRange(
+    id=('date_range_{}').format(tab_name),
+    min_date_allowed=datac.index.round('D').min(),
+    max_date_allowed=datac.index.round('D').max(),
+    initial_visible_month=datac.index.min(),
+    start_date=datac.index.min(),
+    end_date=datac.index.max(),style={'Height':10}
+    )
+    
+def __road_picker(dcc,datac,tab_name):
+    return dcc.Dropdown(datac.Road.sort_values().unique(),'A1',id=('road_select_{}').format(tab_name),style={'width': '25%'})
+    
+def __split_by_picker(dcc,tab_name):
+    return dcc.RadioItems(['no_split','direction','ampm','shortlong','weekday'], 'no_split',id=('split_by_{}').format(tab_name),inline=True)
+    
+def __city_picker(dcc,data,tab_name):
+    return dcc.Dropdown(data.city.sort_values().unique(),placeholder='Select another city',id=('city_select_{}').format(tab_name),style={'width': '50%'})
+    
+def __weather_type_picker(dcc,data,tab_name):
+    return dcc.RadioItems(options={'temperature_2m':'temperature','rain':'rain'},value= 'temperature_2m',id=('weather_type_{}').format(tab_name),inline=True)
+
+def __trafic_information_picker(dcc,tab_name):
+    return dcc.RadioItems(options={'Duration':'duration','distance':'distance'},value='Duration',id=('trafic_information_{}').format(tab_name),inline=True)
+
+def __statistic_picker(dcc,tab_name):
+    return dcc.RadioItems(['mean','max','min','count'],'mean',id=('statistic_{}').format(tab_name),inline=True)
+
+def __weekday_picker(dcc,tab_name):
+    return dcc.Checklist(['0','1','2','3','4','5','6','all'],['1'],id=('weekday_{}').format(tab_name),inline=True)
+
+def __frequence_picker(dcc,tab_name):
+    return dcc.RadioItems(options={'ME':'month','D':'day','h':'hour'},value='D',id=('frequency_{}').format(tab_name),inline=True)
+
+def __rootcause_picker(dcc,datac,tab_name):
+    return dcc.Checklist(datac.Oorzaak_4.unique(),datac.Oorzaak_4.unique().tolist(),id=('rootcause_{}').format(tab_name),inline=True)
--- ALTEN/Files/AIweerfiledashboard/Subfunctions/callbacks.py
+++ ALTEN/Files/AIweerfiledashboard/Subfunctions/callbacks.py
@@ -4,18 +4,18 @@ Created on Fri Feb  7 14:16:27 2025
 
 @author: rbijman
 """
-
 def trafic(callback,Output,Input,pd,px,datac):
+    tab_name = 'trafic'
 
-    __select_dates(callback,Output,Input,pd,'date_range_trafic','month_select_trafic')
+    __select_dates(callback,Output,Input,pd,('date_range_{}').format(tab_name),('month_select_{}').format(tab_name))
 
     @callback(
-        Output('graph_trafic1','figure'), 
-        Output('graph_trafic2','figure'),
-        Input('date_range_trafic','start_date'),
-        Input('date_range_trafic','end_date'),
-        Input('road_select_trafic','value'),
-        Input('split_by_trafic','value')
+        Output(('graph_{}1').format(tab_name),'figure'), 
+        Output(('graph_{}2').format(tab_name),'figure'),
+        Input(('date_range_{}').format(tab_name),'start_date'),
+        Input(('date_range_{}').format(tab_name),'end_date'),
+        Input(('road_select_{}').format(tab_name),'value'),
+        Input(('split_by_{}').format(tab_name),'value')
         )
     def update_output(start_date,end_date,road,split_by):
         if split_by=='no_split':
@@ -24,29 +24,73 @@ def trafic(callback,Output,Input,pd,px,datac):
         fig2 = __plot_files_date(pd,px,datac,start_date,end_date,split_by,road)
         return fig1, fig2
             
-def weather(callback,Output,Input,pd,px,data,go,datac):
-
-    __select_dates(callback,Output,Input,pd,'date_range_weather','month_select_weather')
+def weather(callback,Output,Input,pd,px,data,datac):
+    tab_name = 'weather_and_trafic'
+    __select_dates(callback,Output,Input,pd,('date_range_{}').format(tab_name),('month_select_{}').format(tab_name))
 
     @callback(
-        Output('graph_weather','figure'), 
-        Input('date_range_weather','start_date'),
-        Input('date_range_weather','end_date'),
-        Input('city_select','value'),
-        Input('weather_type','value'),
-        Input('road_select_weather','value'),
-        Input('split_by_weather','value')
+        Output(('graph_{}').format(tab_name),'figure'), 
+        Input(('date_range_{}').format(tab_name),'start_date'),
+        Input(('date_range_{}').format(tab_name),'end_date'),
+        Input(('city_select_{}').format(tab_name),'value'),
+        Input(('weather_type_{}').format(tab_name),'value'),
+        Input(('road_select_{}').format(tab_name),'value'),
+        Input(('split_by_{}').format(tab_name),'value')
         )
     def update_output(start_date,end_date,city,weather_type,road,split_by):
         from plotly.subplots import make_subplots
+        if city is None:
+            city = datac.query('Road==@road').traject_city.value_counts().index[0]
         if split_by=='no_split':
             split_by=None  
         fig2 = px.line(data.loc[start_date:end_date].query('city==@city')[weather_type],color_discrete_sequence=['green'])                        
         fig2.update_traces(yaxis='y2')
         subfig = make_subplots(specs=[[{"secondary_y": True}]])
         fig1 = __plot_files_date(pd,px,datac,start_date,end_date,split_by,road)
-        subfig.add_traces(fig1.data + fig2.data)        
+        subfig.add_traces(fig1.data + fig2.data)   
+        subfig.update_layout(title_x=0.5,title_text=('Number of trafic on {} between {} and {} vs {} in {}').format(road,pd.to_datetime(start_date).date(),pd.to_datetime(end_date).date(),weather_type,city))        
         return subfig
+    
+    
+def further(callback,Output,Input,pd,px,data,datac):
+    tab_name = 'further'
+    __select_dates(callback,Output,Input,pd,('date_range_{}').format(tab_name),('month_select_{}').format(tab_name))
+ 
+    @callback(
+        Output(('graph_{}').format(tab_name),'figure'), 
+        Input(('date_range_{}').format(tab_name),'start_date'),
+        Input(('date_range_{}').format(tab_name),'end_date'),
+        Input(('city_select_{}').format(tab_name),'value'),
+        Input(('weather_type_{}').format(tab_name),'value'),
+        Input(('road_select_{}').format(tab_name),'value'),
+        Input(('trafic_information_{}').format(tab_name),'value'),
+        Input(('statistic_{}').format(tab_name),'value'),
+        Input(('weekday_{}').format(tab_name),'value'),
+        Input(('frequency_{}').format(tab_name),'value'),
+        Input(('rootcause_{}').format(tab_name),'value')
+        )
+    def update_output(start_date,end_date,city,weather_type,road,trafic_information,statistic,weekday,frequency,rootcause):
+        from plotly.subplots import make_subplots
+        if city is None:
+            city = datac.query('Road==@road').traject_city.value_counts().index[0]
+        if 'all' in weekday:
+            weekday = ['0','1','2','3','4','5','6']
+        plot_data = datac[datac.Oorzaak_4.isin(rootcause)].loc[start_date:end_date].reset_index().query('Road==@road').resample(frequency,on='DateTimeStart')[trafic_information].agg(statistic)
+        plot_data = __shift_time_index(plot_data,frequency)
+        plot_data = plot_data[plot_data.index.weekday.astype('str').isin(weekday)]
+        fig1 = px.bar(plot_data,text=plot_data.index.weekday.astype('str'))#,color_discrete_sequence=['green','red','blue','goldenrod','magenta','orange','brown'])#,color=plot_data.index.weekday.astype('str'))      
+        fig1.update_layout(title_x=0.5,barmode='overlay',showlegend=True)
+        fig1.update_traces(opacity=0.5,width=__define_bar_width(frequency))
+        # plot_data2 = data.loc[start_date:end_date].reset_index().query('city==@city').resample('24h',on='dateandtime')[weather_type].mean()
+        # plot_data2 = plot_data2[plot_data2.index.weekday.astype('str').isin(weekday)]
+        fig2 = px.line(data[data.index.weekday.astype('str').isin(weekday)].loc[start_date:end_date].query('city==@city')[weather_type],color_discrete_sequence=['green'])                        
+        # fig2 = px.line(plot_data2,color_discrete_sequence=['green'])
+        fig2.update_traces(yaxis='y2')
+        subfig = make_subplots(specs=[[{"secondary_y": True}]])
+        subfig.add_traces(fig2.data + fig1.data)
+        subfig.update_xaxes({'range': (pd.to_datetime(start_date), pd.to_datetime(end_date))})
+        subfig.update_layout(title_x=0.5,title_text=('{} trafic {} on {} between {} and {} vs {} in {}').format(statistic,trafic_information,road,pd.to_datetime(start_date).date(),pd.to_datetime(end_date).date(),weather_type,city))        
+        return subfig 
     
 
 #Helper functions
@@ -59,7 +103,7 @@ def __select_dates(callback,Output,Input,pd,date_range_id,month_select_id):
         )
     def update_start_date(month):
         start_date= pd.to_datetime(('2024-{}').format(month))
-        end_date = start_date+pd.DateOffset(months=1)
+        end_date = start_date+pd.DateOffset(months=1)-pd.DateOffset(days=1)
         return start_date, end_date
     
 def __plot_files_HMpaal(pd,px,datac,start_date,end_date,split_by,road):
@@ -89,5 +133,28 @@ def __plot_files_date(pd,px,datac,start_date,end_date,split_by,road):
         fig['data'][0]['showlegend']=True
         fig['data'][0]['name']='Number of Trafics'
     return fig
+
+def __define_bar_width(frequency):
+    match frequency:
+        case 'ME':
+            return 1000*3600*24*30*0.8
+        case 'D':
+            return 1000*3600*24*0.8
+        case 'h':
+            return 1000*3600
+        
+def __shift_time_index(plot_data,frequency):
+    match frequency:
+        case 'ME':
+            return plot_data.shift(14,freq='D')
+        case 'D':
+            return plot_data.shift(12,freq='h')
+        case _:
+            return plot_data
+    
+    
+    
+    
+    
     
     
