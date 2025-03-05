5027e392ce4b9c8ec377498960fab463b4511e9a
rbijman <rik-bijman@hotmail.com>
2025-03-05 15:47:33 +0100

    Added update sql database and pickles functionality

--- ALTEN/Files/AIweerfiledashboard/Subfunctions/AIweerfile_functions.py
+++ ALTEN/Files/AIweerfiledashboard/Subfunctions/AIweerfile_functions.py
@@ -45,11 +45,11 @@ def collect_and_clean_AIweerfiledata(start_date,end_date,ampm_border,shortlong_b
     datac[['trajectA','trajectB']] = datac['traject'].str.split(' - ',n=1,expand=True)
 
     #Find the lat_lon_coordinates per city 
-    lat_lon_df = get_lat_lon_per_city(datac)
+    lat_lon_df = __get_lat_lon_per_city(datac)
     datac['traject_city'] = datac.apply(lambda x: x.trajectA if x.trajectA in lat_lon_df.city.values else x.trajectB,axis=1)
 
     #Find the weather per city
-    weather_per_city = get_weather_per_city(lat_lon_df, ["temperature_2m","rain","snowfall","precipitation","wind_speed_10m","direct_radiation","sunshine_duration"],start_date,end_date)
+    weather_per_city = __get_weather_per_city(lat_lon_df, ["temperature_2m","rain","snowfall","precipitation","wind_speed_10m","direct_radiation","sunshine_duration"],start_date,end_date)
 
     # add weather information
     datac = pd.merge(left=datac,right=weather_per_city,left_on=['traject_city','datetime_rounded'],right_on=['city','date'],how='left')
@@ -65,7 +65,36 @@ def collect_and_clean_AIweerfiledata(start_date,end_date,ampm_border,shortlong_b
 
     return datac, lat_lon_df, weather_per_city
 
-def get_lat_lon_per_city(datac):
+def update_sql_database(working_dir,AIweerfile_functions,data_to_update,sql_table_name):
+    from sqlalchemy import create_engine
+    pysql = __import_python_sql_class(working_dir)
+    my_pypg = pysql(working_dir + r"\database_config.ini",'postgresql')
+    engine = create_engine(f"postgresql+psycopg2://{my_pypg.db_info['user']}:{my_pypg.db_info['password']}@{my_pypg.db_info['host']}:{my_pypg.db_info['port']}/{my_pypg.db_info['dbname']}")
+    data_to_update.to_sql(sql_table_name, engine, if_exists='replace')
+    print("SQL database updated")
+
+def update_pickle_with_sql_database_data(AIweerfile_functions,working_dir,pickle_file_path,sql_table_name):
+    import os
+    import time
+    
+    pysql = __import_python_sql_class(working_dir)
+    my_pypg = pysql(working_dir + r"\database_config.ini",'postgresql')
+    
+    data = my_pypg.generic_get_query(f"SELECT * FROM {sql_table_name}")
+    data.to_pickle(pickle_file_path)
+    print(f'{pickle_file_path} updated')
+    last_update = ('last_update done: {}').format(time.ctime(os.path.getmtime(pickle_file_path)))
+    print_message = pickle_file_path + ' updated' + '/n' + last_update
+    return print_message
+
+def getHPrange(beginHP,endHP):
+    if endHP>beginHP:
+        return np.arange(beginHP,endHP,0.1).round(2)
+    else:
+        return np.arange(endHP,beginHP,0.1).round(2)
+    
+#%% private methods
+def __get_lat_lon_per_city(datac):
     cities = []
     lat = []
     lon = []
@@ -73,7 +102,7 @@ def get_lat_lon_per_city(datac):
     for city in unique_city:
         if not city in cities:
             try:            
-                lat_temp,lon_temp = get_lat_lon(city.lower())
+                lat_temp,lon_temp = __get_lat_lon(city.lower())
 
                 lat.append(lat_temp)
                 lon.append(lon_temp)
@@ -88,13 +117,13 @@ def get_lat_lon_per_city(datac):
     lat_lon_df['lon'] = lat_lon_df.lon.astype('float')
     return lat_lon_df
 
-def get_weather_per_city(lat_lon_df,weather_type,start_date,end_date):
+def __get_weather_per_city(lat_lon_df,weather_type,start_date,end_date):
     om = __cache_temp_data()
-    weather_per_city_temp = get_weather_from_api(om,lat_lon_df.lat.astype('str').to_list(), lat_lon_df.lon.astype('str').to_list(), start_date, end_date, lat_lon_df.city,weather_type)
+    weather_per_city_temp = __get_weather_from_api(om,lat_lon_df.lat.astype('str').to_list(), lat_lon_df.lon.astype('str').to_list(), start_date, end_date, lat_lon_df.city,weather_type)
     weather_per_city = weather_per_city_temp.reset_index().merge(lat_lon_df,left_on='city',right_on='city')
     return weather_per_city
 
-def get_lat_lon(your_loc):
+def __get_lat_lon(your_loc):
     import requests
 
     result_city = requests.get(url='https://geocoding-api.open-meteo.com/v1/search?name=' + your_loc)
@@ -104,7 +133,7 @@ def get_lat_lon(your_loc):
     lon = str(list(filter(lambda city: city['country_code']=='NL',location['results']))[0]['longitude'])
     return(lat,lon)    
 
-def get_weather_from_api(om,lat,long,start_date,end_date,cities,weather_types):
+def __get_weather_from_api(om,lat,long,start_date,end_date,cities,weather_types):
     city_df = pd.DataFrame()
 
     # Make sure all required weather variables are listed here
@@ -150,14 +179,10 @@ def get_weather_from_api(om,lat,long,start_date,end_date,cities,weather_types):
     else:
         return hourly_dataframe
 
-def getHPrange(beginHP,endHP):
-    if endHP>beginHP:
-        return np.arange(beginHP,endHP,0.1).round(2)
-    else:
-        return np.arange(endHP,beginHP,0.1).round(2)
 
 
-#%% private methods
+
+
 def __cache_temp_data():
 
     import openmeteo_requests
@@ -171,3 +196,8 @@ def __cache_temp_data():
     
     return om
 
+def __import_python_sql_class(working_dir):
+    import sys
+    sys.path.insert(0,working_dir + r"\SQL")
+    from python_postgress_class import python_postgres as pypg
+    return pypg
